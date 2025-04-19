87c3bb658389e13f410d0195eaa08541f06680cc
Your Name <you@example.com>
2025-04-19 05:40:26 +0000

    sway fix again

--- dotuser/sway/scripts/auto-start.bash
+++ dotuser/sway/scripts/auto-start.bash
@@ -2,9 +2,9 @@
 swaymsg workspace 1 && swaymsg exec 'xfce4-terminal -x notes.bash'
 sleep 1
 
-swaymsg workspace 2 && swaymsg exec gtk-launch ~/.local/share/applications/firefox-dev.desktop
+swaymsg workspace 2 && swaymsg exec gtk-launch firefox-dev.desktop
 sleep 1
 
-swaymsg workspace 3 && swaymsg exec gtk-launch ~/.local/share/applications/firefox-esr.desktop
+swaymsg workspace 3 && swaymsg exec gtk-launch firefox-esr.desktop
 sleep 1
 
