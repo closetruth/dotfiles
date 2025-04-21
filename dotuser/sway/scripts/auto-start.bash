a6b9f7d3126558b8f24a436d6ec12f813fd871be
Your Name <you@example.com>
2025-04-21 00:45:11 +0000

    sway optimize

--- dotuser/sway/scripts/auto-start.bash
+++ dotuser/sway/scripts/auto-start.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 swaymsg workspace 1 && swaymsg exec 'xfce4-terminal -x notes.bash'
-sleep 1
+sleep 2
 
 swaymsg workspace 2 && swaymsg exec gtk-launch firefox-dev.desktop
-sleep 1
+sleep 2
 
 swaymsg workspace 3 && swaymsg exec gtk-launch firefox-esr.desktop
-sleep 1
+sleep 2
 
